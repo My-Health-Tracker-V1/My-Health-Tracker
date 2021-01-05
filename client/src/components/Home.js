aae5277dfa3cfbaae4a30c4d0ae26ade9421d581
Hortencia Jorquera <hortencia.pj@gmail.com>
2021-01-05 10:33:17 +0100

    fix rendering of dashboard after changes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,84 +1,25 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {logout} from '../services/auth'
-import TopBar from './shared/TopBar'
+import React from "react";
+import { Link } from "react-router-dom";
+import TopBar from "./shared/TopBar";
 
-
-const handleLogout = props => {
-  console.log(props);
-  logout().then(() => {
-    props.setUser(null);
-  })
-}
-
-export default function Home(props) {
+export default function Home() {
   return (
     <div>
-      <TopBar icon='health-icon' title='My Health Diary'/>
+      <TopBar icon="health-icon" title="My Health Diary" />
       <div className="flex flex-column justify-center items-center pv6">
-          <Link className="f5 link dim br-pill ba bw2 ph3 pv2 mb3 dib dark-blue w4" to="/signup">Signup</Link>
-          <Link className="f5 link dim br-pill ba bw2 ph3 pv2 mb2 dib dark-blue w4" to="/login">Login</Link>
+        <Link
+          className="f5 link dim br-pill ba bw2 ph3 pv2 mb3 dib dark-blue w4"
+          to="/signup"
+        >
+          Signup
+        </Link>
+        <Link
+          className="f5 link dim br-pill ba bw2 ph3 pv2 mb2 dib dark-blue w4"
+          to="/login"
+        >
+          Login
+        </Link>
       </div>
     </div>
-  )
+  );
 }
-
-
-
-// export default class Home extends Component {
-
-//   state = {
-//     showSignup: false,
-//     showLogin: false
-//   }
-
-//   handleSignupClick = () => {
-//     this.setState({
-//       showSignup: !this.state.style
-//     })
-//   }
-
-//   handleLoginClick = () => {
-//     this.setState({
-//       showLogin: !this.state.showLogin
-//     })
-//   }
-
-//   render() {
-//     return (
-//       <div style={{"height": "700px","display": "flex", "flexDirection": "column", "justifyContent": 'center', "alignItems": "center"}}>
-
-//       <nav className='ba blue pv2 w-100 fixed top-2' style={{"marginBottom": "10px"}}>
-//           <div className="link blue hover-silver dib mh3 tc" style={{
-//             "display": "flex", "flexDirection":"row", "justifyContent": "center", "alignItems":"center"}}>
-            
-//             <Icons icon='health-icon'/>
-//             <span class="f6 db">My Health Diary</span>
-//           </div>
-//       </nav>
-    
-//       <div style={{"display": "flex", "flexDirection": "column", "justifyContent": 'center', "alignItems": "center"}}>
-//         <button onClick={this.handleSignupClick} className="f8 link dim br-pill ba bw1 ph5 pv2 mb4 dib dark-blue" >Signup</button>
-//         <button onClick={this.handleLoginClick} className="f8 link dim br-pill ba bw1 ph5 pv2 mb2 dib dark-blue" >Login</button>
-//       </div>
-
-//       <div>
-//         {
-//           this.state.showSignup === true && 
-//           <Signup />
-//         }
-//       </div>
-
-//       <div>
-//         {
-//           this.state.showLogin === true &&
-//           <Login />
-//         }
-//       </div>
-      
-//     </div>
-//     )
-//   }
-// }
-
-
--- client/src/components/nav-bar-options/Settings.js
+++ client/src/components/nav-bar-options/Settings.js
@@ -1,68 +1,98 @@
-import React from 'react'
-import { logout } from '../../services/auth';
-import { Link } from 'react-router-dom';
-import TopBar from '../shared/TopBar'
-import BottomNavbar from '../shared/BottomNavbar'
-import Icons from '../shared/Icons';
-import axios from 'axios'
+import React from "react";
+import { logout } from "../../services/auth";
+import { Link } from "react-router-dom";
+import TopBar from "../shared/TopBar";
+import BottomNavbar from "../shared/BottomNavbar";
+import Icons from "../shared/Icons";
+import axios from "axios";
 
 export default class Settings extends React.Component {
+  
+  state = {
+    newEmail: "",
+    newPassword: "",
+  };
 
-  state={
-    newEmail:'',
-    newPassword:''
-  }
-
-  handleChange=event=>{
-    const { name, value }= event.target;
+  handleChange = (event) => {
+    const { name, value } = event.target;
 
     this.setState({
-    [name]:value
-    })
-  }
+      [name]: value,
+    });
+  };
 
-  handleSubmit=event=>{
-
+  handleSubmit = (event) => {
     event?.preventDefault();
-    
-    const {newEmail,newPassword}=this.state;
 
-    axios.put(`/api/users/${this.props.user._id}`,{data:[newEmail,newPassword]})
-      .then(res=>{
-        console.log(res)
+    const { newEmail, newPassword } = this.state;
+
+    axios
+      .put(`/api/users/${this.props.user._id}`, {
+        data: [newEmail, newPassword],
       })
-      .catch(err=>console.log(err))
-  }
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
 
   handleLogout = () => {
-
     logout().then(() => {
       this.props.setUser(null);
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div>
-        <TopBar icon="settings" title="Settings"/>
+        <TopBar icon="settings" title="Settings" />
 
-        <form onSubmit={this.handleSubmit} className='flex flex-column items-center' action="POST">
-          <label className="f6 mt3" htmlFor="new-email">New email:</label>
-          <input className="mb2" id="new-email" name="newEmail" onChange={this.handleChange} value={this.state.newEmail} type="text"/>
-          <label className="f6 mt3" htmlFor="newPassword">New password:</label>
-          <input className="mb2" id="new-password" name="newPassword" onChange={this.handleChange} value={this.state.newPassword} type="text"/>
-          <button className="f6 w4 dim ph3 pv2 mt3 dib white bg-dark-green br-pill b--dark-green" type="submit">Update</button>
+        <form
+          onSubmit={this.handleSubmit}
+          className="flex flex-column items-center"
+          action="POST"
+        >
+          <label className="f6 mt3" htmlFor="new-email">
+            New email:
+          </label>
+          <input
+            className="mb2"
+            id="new-email"
+            name="newEmail"
+            onChange={this.handleChange}
+            value={this.state.newEmail}
+            type="text"
+          />
+          <label className="f6 mt3" htmlFor="newPassword">
+            New password:
+          </label>
+          <input
+            className="mb2"
+            id="new-password"
+            name="newPassword"
+            onChange={this.handleChange}
+            value={this.state.newPassword}
+            type="text"
+          />
+          <button
+            className="f6 w4 dim ph3 pv2 mt3 dib white bg-dark-green br-pill b--dark-green"
+            type="submit"
+          >
+            Update
+          </button>
         </form>
 
-        <Link to='/' onClick={() => this.handleLogout()}>
+        <Link to="/" onClick={() => this.handleLogout()}>
           <div className="blue pt5">
-            <Icons icon="logout"/>
+            <Icons icon="logout" />
           </div>
-          <p className="f6 w4 dim ph3 pv2 mt3 dib white bg-dark-blue br-pill b--dark-blue">Logout</p>
+          <p className="f6 w4 dim ph3 pv2 mt3 dib white bg-dark-blue br-pill b--dark-blue">
+            Logout
+          </p>
         </Link>
 
-      <BottomNavbar />
+        <BottomNavbar />
       </div>
-    )
+    );
   }
 }
